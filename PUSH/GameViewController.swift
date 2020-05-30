36225a3185dde2beffdbe284a094138160b285fc
Elliott <elliottnb55@gmail.com>
2020-05-30 20:42:59 -0230

    GameComplete

--- PUSH/GameViewController.swift
+++ PUSH/GameViewController.swift
@@ -9,15 +9,29 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
 class GameViewController: UIViewController {
 
+    var backgroundMusicPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
+                
+                let tooComfortable = Bundle.main.url(forResource: "TooComfortable", withExtension: "mp3")
+                do {
+                    try backgroundMusicPlayer = AVAudioPlayer(contentsOf: tooComfortable!)
+                }catch {
+                    print("Can't play music")
+                }
+                
+                backgroundMusicPlayer.numberOfLoops = -1
+                backgroundMusicPlayer.prepareToPlay()
+                backgroundMusicPlayer.play()
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
